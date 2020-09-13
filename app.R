798d4c3a2888286a30bac912336ed5d99e1ee809
Pedro Pacheco <pedrohmp36@gmail.com>
2020-09-12 21:53:32 -0300

    creating sidebarpanel

--- app.R
+++ app.R
@@ -1,50 +1,18 @@
-library(shiny)
-library(ggplot2)
-library(tidyverse)
+source("./src/config/index.R")
 
-bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
-
-ui <- fluidPage(
-  titlePanel("BC Liquor Store prices"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("priceInput", "Price", min = 0, max = 100, value = c(25, 40), pre = "$"),
-      radioButtons("typeInput", "Product type", choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"), selected = "WINE"),
-      selectInput("countryInput", "Country", choices = c("CANADA", "FRANCE", "ITALY"))
-    ),
-    mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
-    )
+ui <- tags$html(
+  tags$head(
+    useShinyjs(),
+    includeScript("./scripts/reload.js")
+  ),
+  tags$body(
+    router_ui()
   )
 )
 
 server <- function(input, output, session) {
-  filtered <- reactive({
-    req(input$priceInput, input$typeInput, input$countryInput)
-    
-    bcl %>%
-      filter(
-        Price >= input$priceInput[1],
-        Price <= input$priceInput[2],
-        Type == input$typeInput,
-        Country == input$countryInput
-      )
-  })
-  
-  output$coolplot <- renderPlot({
-    req(filtered)
-    
-    ggplot(filtered(), aes(Alcohol_Content)) + geom_histogram()
-  })
-  
-  output$results <- renderTable({
-    req(filtered)
-    
-    filtered()
-  })
-  
+  router(input, output, session)
 }
 
+options(shiny.port = 3333)
 shinyApp(ui, server)
---
+++ src/pages/Home/index.R
@@ -0,0 +1,8 @@
+uiHome <- tags$div(
+  class = "home",
+  sideBarPanel(id = "sideBarPanelHome")
+)
+
+serverHome <- function(input, output, session) {
+  sideBarPanelServer(id = "sideBarPanelHome")
+}
